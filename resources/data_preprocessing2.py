6af7e783b150c204f694b905f03b7a59eaf3e353
DAMPER <44198800+dampers@users.noreply.github.com>
2022-05-15 21:14:05 +0900

    Update data_preprocessing2.py
    fix make test data

--- resources/data_preprocessing2.py
+++ resources/data_preprocessing2.py
@@ -461,7 +461,7 @@ def _make_test(test_file):
         hseq = line[5].rstrip().split(',')
         hlt = line[6].rstrip().split(',')
         htime = line[7].rstrip().split(',')
-        mlen = (len(hseq) // 3) * 2
+        mlen = min((len(hseq) * 2) // 3, 1)
         for i in range(mlen, len(hseq)):
             next_problem = hseq[i]
             next_lt = hlt[i]
@@ -469,7 +469,7 @@ def _make_test(test_file):
             output.append('\t'.join(['1', user_id, next_problem, next_lt, next_time, ','.join(hseq[0:i]), ','.join(hlt[0:i]), ','.join(htime[0:i])]))
     
     output.append(last)
-    f = open(test_file+"_test", 'w', encoding='utf-8')
+    f = open(test_file, 'w', encoding='utf-8')
     f.write('\n'.join(output))
     f.close()
 
